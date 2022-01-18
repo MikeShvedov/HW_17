53e29b87486b33b341a75002ed4526bd485f49ed
MikeShvedov <michel.chvedov@mail.ru>
2022-01-18 20:35:06 +0300

    First commit ever
    Test commit

---
+++ Assets/PlatformMovement.cs
@@ -0,0 +1,26 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlatformMovement : MonoBehaviour
+//{
+//    public SliderJoint2D sliderRef;
+//    private JointMotor2D motorRef;
+//    JointMotor2D motor = new JointMotor2D { motorSpeed = -2, maxMotorTorque = 10 };
+
+//    void Start()
+//    {
+//         motorRef = GetComponent<JointMotor2D>();
+//    }
+
+//    private void Collider2D(Collider trig)
+//    {
+//        if (trig.CompareTag("Ground"))
+//        {
+//            motor = 
+//        }
+//    }
+
+    
+  
+//}
